085cc09cc906fdda3c4dbb6c73c4b1295f07f6ef
Anders Newgard <anne9707@colorado.edu>
2023-12-04 16:52:42 -0500

    basic css loader implemented

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -33,6 +33,12 @@ module.exports = () => {
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+
+        
         
       ],
     },
